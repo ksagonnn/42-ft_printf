5ade60f98ed4a3fca44e3ed531f847533dd1d35f
ksagon <87269186+ksagonnn@users.noreply.github.com>
2022-01-20 12:03:55 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -3,131 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksagon <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*   By: ksagon <ksagon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 15:53:12 by ksagon            #+#    #+#             */
-/*   Updated: 2022/01/10 15:37:54 by ksagon           ###   ########.fr       */
+/*   Updated: 2022/01/19 13:57:28 by ksagon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdio.h>
-#include<unistd.h>
-#include<stdarg.h>
+#include "libft.h
+"
+char	detect_arg(const char *str, int i)
+{
+		if (str[i] == '%' && (str[i + 1] == 'c' || str[i] == 's' 
+		|| str[i] == 'p' || str[i] == 'd' || str[i] == 'i' 
+		|| str[i] == 'u' || str[i] == 'x' || str[i] == 'X' 
+		|| str[i] == '%'))
+	return (str[i + 1]);
+return (0);
+}
 
 int	ft_printf(const char *input, ...)
+
+int ft_printf_args(char convert, va_list args)
 {
-	va_list;
-	va_start(args, input);
-	char c;
+	int i;
 
-	c = 0;
-	if (c != "%")
-		write (1, c, 1);
-	{
-		char d;
-
-	d = 0;
-	if (d == '%')
-		d++;
-	return (d + 1);
-		{
-			param = (d + 1);
-			if (param == 'c', 's', 'p', 'd', 'i', 'u', 'x', 'X', '%')
-				return (ft_printf_args(param));
-			else if
-				return (0);
-		}
-	}
-}
-
-void ft_printf_args(char convert, va_list args, int *rcount)
-{
-	if (param == 'c')
-		return (ft_printf_char(param));
-	if (param == 's')
-		return (ft_printf_str(param));
-	if (param == 'p')
-		return (ft_printf_ptr(param));
-	if (param == 'd' || param == 'i')
-		return (ft_printf_int(param));
-	if (param == 'u')
-		return (ft_printf_dun(param));
-	if (param == 'x' || param == 'X')
-		return (ft_printf_hex(param));
-}
-
-int ft_printf_char(int c, int *rcount)
-{
-	return (ft_putchar((c) + 1));
-}
-
-int ft_printf_str(char *str, int *rcount)
-{
-	if (c == NULL)
-		return (ft_putstr_fd(NULL));
-	else if
-		return (ft_putstr_fd(str));
-}
-int ft_printf_ptr(char *ptr, int *rcount)
-int ft_printf_int(int nbr, int *rcount)
-{
-	if (nbr <= 10 || nbr >= 0)
-		return (ft_putnbr_fd(nbr));
-}
-
-int ft_printf_dun(unsigned int nbr, int *rcount)
-int ft_printf_hex(unsigned long nbr, int *rcount, int format)
-
-
-
-
-
-//p unsigned long PAREIL
-
-
-
-
-
-
-//%p (imprime en hexa un pointeur void*)
-static void decToHexa(int n)
-{
-    {
-        // char array to store
-        // hexadecimal number
-        char[] hexaDeciNum = new char[100];
- 
-        // counter for hexadecimal number array
-        int i = 0;
-        while (n != 0) 
-		{
-            // temporary variable to store remainder
-            int temp = 0;
- 
-            // storing remainder in temp variable
-            temp = n % 16;
- 
-            // check if temp < 10
-            if (temp < 10) {
-                hexaDeciNum[i] = (char)(temp + 48);
-                i++;
-            }
-            else {
-                hexaDeciNum[i] = (char)(temp + 55);
-                i++;
-            }
- 
-            n = n / 16;
-        }
-        // printing hexadecimal number
-        // array in reverse order
-        for (int j = i - 1; j >= 0; j--)
-            Console.Write(hexaDeciNum[j]);
-    }
-    // Driver Code
-    public static void Main(String[] args)
-    {
-        int n = 2545;
-        decToHexa(n);
-    }
+	i = 0;
+	if (convert == 'c')
+		i = ft_printf_char(va_arg(args, char);
+	else if (convert == 's')
+		i = ft_putstr_fd(va_arg(args, int),1);
+	else if (convert == 'p')
+		i = ft_printf_ptr(va_arg(args, unsigned long));
+	else if (convert == 'd' || convert == 'i')
+		i = ft_printf_int(va_arg(args, int));
+	else if (convert == 'u')
+		i = ft_printf_dun(va_arg(args, unsigned int));
+	else if (convert == 'x' || convert == 'X')
+		i = ft_printf_hex(va_arg(args, unsigned long));
+	else if (convert == '%')
+		i = ft_printf_percent('%');
 }
